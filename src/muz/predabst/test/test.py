7725b62106da28582a04839e7d0487b537eb6410
jchl <jchl@microsoft.com>
2015-02-17 14:02:06 +0000

    predabst: perform some basic output validation even for tests with no reference output.  Keep track of the number of tests that pass/fail.

--- src/muz/predabst/test/test.py
+++ src/muz/predabst/test/test.py
@@ -9,7 +9,8 @@ import difflib
 OUTFILE = "test.log"
 Z3TRCFILE = ".z3-trace"
 TRCFILE = "test.trc"
-Z3CMD = [r"..\..\..\..\build\z3", "-smt2", "fixedpoint.engine=predabst", "-dbg:predabst", "-tr:predabst"]
+testpydir = os.path.dirname(__file__)
+Z3CMD = [os.path.join(testpydir, r"..\..\..\..\build\z3"), "-smt2", "fixedpoint.engine=predabst", "-dbg:predabst", "-tr:predabst"]
 
 filter = sys.argv[1:]
 
@@ -18,9 +19,15 @@ def writeHeader(f, filename):
     print >>f, ">>> Running %s" % filename
     f.flush()
 
-def writeFooter(f, status):
-    print >>f, ">>> %s" % status
+def writeFooter(f, msg):
+    print >>f, ">>> %s" % msg
     f.flush()
+
+numPassed = 0
+numFailed = 0
+
+PASSED = object()
+FAILED = object()
 
 with open(OUTFILE, "w") as outfile:
     with open(TRCFILE, "w") as trcfile:
@@ -33,24 +40,48 @@ with open(OUTFILE, "w") as outfile:
                 expectedOutput = file(outFilename).read()
             else:
                 expectedOutput = None
-                
             writeHeader(outfile, inFilename)
             writeHeader(trcfile, inFilename)
             try:
                 output = subprocess.check_output(Z3CMD + [inFilename], stderr=subprocess.STDOUT)
                 if expectedOutput is not None:
                     if expectedOutput.splitlines() == output.splitlines():
-                        status = "VALIDATED"
+                        status = PASSED
+                        msg = "PASSED+"
                     else:
-                        status = "VALIDATION FAILED\n" + "\n".join(difflib.ndiff(expectedOutput.splitlines(), output.splitlines()))
+                        status = FAILED
+                        msg = "FAILED: output not as expected:\n" + "\n".join(difflib.ndiff(expectedOutput.splitlines(), output.splitlines()))
                 else:
-                    status = "PASSED"
+                    if '-sat-' in testname and 'sat' not in output.splitlines():
+                        status = FAILED
+                        msg = "FAILED: didn't find 'sat'"
+                    elif '-unsat-' in testname and 'unsat' not in output.splitlines():
+                        status = FAILED
+                        msg = "FAILED: didn't find 'unsat'"
+                    elif any('leak' in line for line in output.splitlines()):
+                        status = FAILED
+                        msg = "FAILED: memory leak"
+                    else:
+                        status = PASSED
+                        msg = "PASSED"
             except subprocess.CalledProcessError as e:
                 output = e.output
-                status = "FAILED"
+                status = FAILED
+                msg = "FAILED: exited with status %d" % e.returncode
             outfile.write(output)
             with open(Z3TRCFILE, "r") as z3trcfile:
                 shutil.copyfileobj(z3trcfile, trcfile)
-            writeFooter(outfile, status)
-            writeFooter(trcfile, status)
-            print "%s: %s" % (inFilename, status)
+            writeFooter(outfile, msg)
+            writeFooter(trcfile, msg)
+            print "%s: %s" % (inFilename, msg)
+            if status is PASSED:
+               numPassed += 1
+            else:
+               assert status is FAILED
+               numFailed += 1
+
+print
+if numFailed:
+    print "%d of %d test(s) failed" % (numFailed, numPassed + numFailed)
+else:
+    print "All %d test(s) passed" % numPassed
